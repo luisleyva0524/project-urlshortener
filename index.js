68ea1e26c5cdcc62e49aefb4383e7da7cc937f51
luisleyva0524 <luisantonioleyva0524@gmail.com>
2022-10-01 20:50:32 -0700

    done

--- index.js
+++ index.js
@@ -1,24 +1,90 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const dns = require("dns");
+const url = require("url");
 const app = express();
+const mongoose = require("mongoose");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.use(cors());
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const urlSchema = new mongoose.Schema({
+  originalUrl: { type: String },
+  shorturl: Number,
+});
+
+const urlDb = mongoose.model("Directions", urlSchema);
+
+const randomShorturl = Math.floor(Math.random() * 61 + 1);
+var shorturl = "";
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/shorturl", (req, res) => {
+  shorturl = req.body.url;
+  const parseUrl = url.parse(shorturl);
+
+  dns.lookup(parseUrl.hostname, (err) => {
+    if (
+      parseUrl.protocol === null ||
+      parseUrl.hostname === null ||
+      (err && err.code === "ENOTFOUND")
+    ) {
+      // res.json({ error: "invalid url" });
+      res.statusCode(200);
+    } else {
+      const urlModel = new urlDb({
+        originalUrl: shorturl,
+        shorturl: randomShorturl,
+      });
+
+      urlModel.save((err, data) => {
+        if (err) {
+          res.json({ error: "error to save data" });
+        } else {
+          res.json({
+            original_url: data["originalUrl"],
+            shorturl: data["shorturl"],
+          });
+        }
+      });
+    }
+  });
+});
+
+app.get("/api/shorturl/:shorturl", async function(req, res) {
+  const shortUrl = parseInt(req.params.shorturl);
+  try {
+    await urlDb.findOne({shorturl: shortUrl}).then(data => {
+      if (!data) {
+        throw new Error("Short URL not found.");
+      }
+      res.redirect(data.shortUrl);
+    });
+  } catch (e) {
+    res.send({
+      error: e.message
+    });
+  }
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
